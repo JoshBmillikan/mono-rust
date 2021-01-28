739c2442f325b5d0a39b3b7d8ec7c44b055665a5
Josh <jmillikan12345@gmail.com>
2021-01-28 17:57:18 -0500

    put files into modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,43 @@
 
-#[cfg(test)]
-mod tests {
-    use std::env;
-    use std::path::{Path, PathBuf};
+//todo debug-mono-symfile - needs glib.h
+//todo profiler-events requires macro definitions, not sure if portable
+//todo sgen-bridge
+//todo verify.h - needs glib.h as well
 
-    #[test]
-    fn it_works() {
-        let env_path = env::var("MONO_INSTALL_DIR").expect("Could not find MONO_INSTALL_DIR");
-        let path = Path::new(env_path.as_str());
-        let mut headers = Vec::new();
-        get_headers(path,&mut headers);
-        for head in headers {
-            println!("found header: {}",head.to_str().unwrap());
-        }
-        assert_eq!(2 + 2, 4);
-    }
-    fn get_headers(path: &Path, out: &mut Vec<Box<PathBuf>>) {
-        if path.is_file() && path.extension().is_some() && path.extension().unwrap().eq("h") {
-            out.push(Box::new(path.to_path_buf()));
-            return;
-        }
-        else {
-            if path.is_dir() {
-                for p in path.read_dir().unwrap() {
-                    get_headers(p.unwrap().path().as_path(), out);
-                }
-            }
-        }
-    }
+pub mod jit {
+    pub mod jit;
 }
+pub mod metadata {
+    pub mod appdomain;
+    pub mod assembly;
+    pub mod attrdef;
+    pub mod blob;
+    pub mod class;
+    pub mod debug_helpers;
+    pub mod exception;
+    pub mod image;
+    pub mod loader;
+    pub mod metadata;
+    pub mod mono_config;
+    pub mod mono_debug;
+    pub mod mono_gc;
+    pub mod object;
+    pub mod object_forward;
+    pub mod opcodes;
+    pub mod profiler;
+    pub mod reflection;
+    pub mod row_indexes;
+    pub mod threads;
+    pub mod tokentype;
+}
+
+pub mod utils {
+    pub mod mono_counters;
+    pub mod mono_dl_fallback;
+    pub mod mono_error;
+    pub mod mono_forward;
+    pub mod mono_jemalloc;
+    pub mod mono_logger;
+    pub mod mono_publib;
+}
+
